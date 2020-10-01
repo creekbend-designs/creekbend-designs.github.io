2fe6d1e198ace4003ee47c55ddeb4a5faaf9fdd8
Karen Stephenson <creekbendconsulting.gmail.com>
2020-10-01 11:16:56 -0600

    Fix sizing and weather

--- Creekbend/Weather/js/weather.js
+++ Creekbend/Weather/js/weather.js
@@ -7,7 +7,7 @@ var cObj;
 var fObj;
 
 // GET THE CONDITIONS
-weatherConditions.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=80033,us&units=imperial&appid=39f52db394a9548fab312dd08c5734ed', true);
+weatherConditions.open('GET', '//api.openweathermap.org/data/2.5/weather?zip=80033,us&units=imperial&appid=39f52db394a9548fab312dd08c5734ed', true);
 weatherConditions.responseType = 'text';
 weatherConditions.send(null);
 
@@ -23,7 +23,7 @@ weatherConditions.onload = function() {
         document.getElementById('weather').innerHTML = cObj.weather[0].description;
         document.getElementById('temperature').innerHTML = cObj.main.temp + "&deg;";
 
-        var icon_path = "http://openweathermap.org/img/w/" + cObj.weather[0].icon + ".png";
+        var icon_path = "//openweathermap.org/img/w/" + cObj.weather[0].icon + ".png";
         document.getElementById('icon').src = icon_path;
     
         if (whichFile === "Creekbend Weather") {
@@ -44,7 +44,7 @@ weatherConditions.onload = function() {
 
 
 // GET THE FORECARST
-weatherForecast.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?zip=80033,us&units=imperial&appid=39f52db394a9548fab312dd08c5734ed', true);
+weatherForecast.open('GET', '//api.openweathermap.org/data/2.5/forecast?zip=80033,us&units=imperial&appid=39f52db394a9548fab312dd08c5734ed', true);
 weatherForecast.responseType = 'text'; 
 weatherForecast.send();
 
@@ -61,7 +61,7 @@ if (weatherForecast.status === 200){
     /* date_raw = date_raw.substring(5); */
     date_raw = date_raw.substring(5,11);
     document.getElementById('r1c1').innerHTML = date_raw;
-    var icon_path = "http://openweathermap.org/img/w/" + fObj.list[0].weather[0].icon + ".png";
+    var icon_path = "//openweathermap.org/img/w/" + fObj.list[0].weather[0].icon + ".png";
     document.getElementById('r1c2').src = icon_path;
     document.getElementById('r1c3').innerHTML = fObj.list[0].main.temp_min + "&deg;";
     document.getElementById('r1c4').innerHTML = fObj.list[0].main.temp_max + "&deg;";
@@ -69,7 +69,7 @@ if (weatherForecast.status === 200){
     var date_raw = fObj.list[8].dt_txt;
     date_raw = date_raw.substring(5,11);
     document.getElementById('r2c1').innerHTML = date_raw;
-    var icon_path = "http://openweathermap.org/img/w/" + fObj.list[8].weather[0].icon + ".png";
+    var icon_path = "//openweathermap.org/img/w/" + fObj.list[8].weather[0].icon + ".png";
     document.getElementById('r2c2').src = icon_path;
     document.getElementById('r2c3').innerHTML = fObj.list[8].main.temp_min + "&deg;";
     document.getElementById('r2c4').innerHTML = fObj.list[8].main.temp_max + "&deg;";
@@ -77,7 +77,7 @@ if (weatherForecast.status === 200){
     var date_raw = fObj.list[16].dt_txt;
     date_raw = date_raw.substring(5,11);
     document.getElementById('r3c1').innerHTML = date_raw;
-    var icon_path = "http://openweathermap.org/img/w/" + fObj.list[16].weather[0].icon + ".png";
+    var icon_path = "//openweathermap.org/img/w/" + fObj.list[16].weather[0].icon + ".png";
     document.getElementById('r3c2').src = icon_path;
     document.getElementById('r3c3').innerHTML = fObj.list[16].main.temp_min + "&deg;";
     document.getElementById('r3c4').innerHTML = fObj.list[16].main.temp_max + "&deg;";
@@ -85,7 +85,7 @@ if (weatherForecast.status === 200){
     var date_raw = fObj.list[24].dt_txt;
     date_raw = date_raw.substring(5,11);
     document.getElementById('r4c1').innerHTML = date_raw;
-    var icon_path = "http://openweathermap.org/img/w/" + fObj.list[24].weather[0].icon + ".png";
+    var icon_path = "//openweathermap.org/img/w/" + fObj.list[24].weather[0].icon + ".png";
     document.getElementById('r4c2').src = icon_path;
     document.getElementById('r4c3').innerHTML = fObj.list[24].main.temp_min + "&deg;";
     document.getElementById('r4c4').innerHTML = fObj.list[24].main.temp_max + "&deg;";
@@ -93,7 +93,7 @@ if (weatherForecast.status === 200){
     var date_raw = fObj.list[32].dt_txt;
     date_raw = date_raw.substring(5,11);
     document.getElementById('r5c1').innerHTML = date_raw;
-    var icon_path = "http://openweathermap.org/img/w/" + fObj.list[32].weather[0].icon + ".png";
+    var icon_path = "//openweathermap.org/img/w/" + fObj.list[32].weather[0].icon + ".png";
     document.getElementById('r5c2').src = icon_path;
     document.getElementById('r5c3').innerHTML = fObj.list[32].main.temp_min + "&deg;";
     document.getElementById('r5c4').innerHTML = fObj.list[32].main.temp_max + "&deg;";
